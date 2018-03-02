48f2c40f4e7d7e29dab2ed6ec4ec3a0e88667c4c
Valentin Clement <clementval@gmail.com>
2018-03-02 11:50:10 +0100

    Add test data

---
+++ F-FrontEnd/test/testdata/issue550.f90
@@ -0,0 +1,23 @@
+MODULE mod1
+  IMPLICIT NONE
+  PRIVATE
+  PUBLIC :: t_state
+
+  TYPE, ABSTRACT :: t_state
+    CLASS(t_State), POINTER :: parent => NULL()
+  CONTAINS
+    PROCEDURE :: init_state
+  END TYPE t_State
+
+CONTAINS
+  SUBROUTINE init_state(this, parent)
+    CLASS(t_state), INTENT(inout), TARGET :: this
+    CLASS(t_state), POINTER, OPTIONAL       :: parent
+  
+    SELECT TYPE(parent)
+      CLASS IS (t_state)
+        this%parent => parent
+    END SELECT
+
+  END SUBROUTINE init_state
+END MODULE mod1
